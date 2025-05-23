cd04f2f0bbc466ced12602f12d485297bc6cd5f6
Hari Aakash K <user@ip-172-31-16-35.ap-south-1.compute.internal>
2025-05-23 11:28:45 +0000

    Updates to send and receive messages fro other computers

--- exercise-1/src/tcp_echo_client.cc
+++ exercise-1/src/tcp_echo_client.cc
@@ -15,8 +15,8 @@ int main(int argc, char* argv[]) {
     if(i > 1) message += " ";
     message += argv[i];
   }
-  const int kPort = 8080;
-  const std::string kServerAddress = "127.0.0.1";
+  const int kPort = 35000;
+  const std::string kServerAddress = "13.200.106.237";
   sockaddr_in address;
   const int kBufferSize = 1024;
   char buffer[kBufferSize] = {0};

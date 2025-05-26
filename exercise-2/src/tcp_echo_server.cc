819606ede66ab0d3785b5f05541cbecf8ccbbf8c
Hari Aakash K <user@ip-172-31-16-35.ap-south-1.compute.internal>
2025-05-26 12:11:43 +0000

    Answered the questions in tcp_echo_server.cc

--- exercise-2/src/tcp_echo_server.cc
+++ exercise-2/src/tcp_echo_server.cc
@@ -78,6 +78,7 @@ void handle_connections(int sock, int port) {
   socklen_t address_size = sizeof(address);
 
   // #Question - is it good to have an infinite loop?
+  // #Answer - Yes. I don't see any other option.
   while (true) {
     int accepted_socket = accept(sock, (sockaddr *)&address, &address_size);
     if (accepted_socket < 0) {
@@ -95,6 +96,7 @@ int main() {
   sockaddr_in address = create_address(kPort);
 
   // #Question - is there a better name for this function?
+  // #Answer - The current name describes it well.
   start_listening_on_socket(my_socket, address);
   std::cout << "Server listening on port " << kPort << "\n";
   handle_connections(my_socket, kPort);

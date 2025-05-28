d47d211edf0d82bbe061bdcd9ab0c319216e5239
Hari Aakash K <user@ip-172-31-16-35.ap-south-1.compute.internal>
2025-05-28 19:50:04 +0000

    Added the definitions for the rest of the functions

--- exercise-5/src2/client.cpp
+++ exercise-5/src2/client.cpp
@@ -28,3 +28,30 @@ Client::Client(const std::string &server_ip, int port) {
   check_error(err_code <= 0, "Invalid address/ Address not supported.\n");
 }
 
+void Client::connect_to_server() {
+  auto err_code =
+      connect(_sock, (sockaddr *)&address, sizeof(address));
+  check_error(err_code < 0, "Connection Failed.\n");
+}
+
+void Client::send_and_receive_message(const std::string &message) {
+  const int kBufferSize = 1024;
+  char recv_buffer[kBufferSize] = {0};
+
+  // Send the message to the server
+  send(_sock, message.c_str(), message.size(), 0);
+  std::cout << "Sent: " << message << "\n";
+
+  // Receive response from the server
+  ssize_t read_size = read(_sock, recv_buffer, kBufferSize);
+  check_error(read_size < 0, "Read error.\n");
+  if (read_size > 0) {
+    std::cout << "Received: " << recv_buffer << "\n";
+  } else if (read_size == 0) {
+    std::cout << "Server closed connection.\n";
+  }
+}
+
+void Client::close_socket() {
+  close(_sock);
+}

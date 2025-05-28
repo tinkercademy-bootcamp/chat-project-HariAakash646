d3802b5590917476c620e159fe0f51fbf9d435a5
Hari Aakash K <user@ip-172-31-16-35.ap-south-1.compute.internal>
2025-05-28 19:59:05 +0000

    CaInitialize cent clobjectnd send message to server

--- exercise-5/src2/tcp_echo_client.cpp
+++ exercise-5/src2/tcp_echo_client.cpp
@@ -8,13 +8,28 @@
 #include <unistd.h>
 #include "client.h"
 
+std::string read_args(int argc, char *argv[]) {
+  std::string message = "Hello from client";
+  if (argc == 1) {
+    std::cout << "Usage: " << argv[0] << " <message>\n";
+    exit(EXIT_FAILURE);
+  }
+  if (argc > 1) {
+    message = argv[1];
+  }
+  return message;
+}
+
 int main(int argc, char *argv[]) {
   const int kPort = 8080;
   const std::string kServerAddress = "127.0.0.1";
 
-  // std::string message = read_args(argc, argv);
+  std::string message = read_args(argc, argv);
 
-  
+  Client client(kServerAddress, kPort);
+
+  client.send_and_receive_message(message);
+  client.close_socket();  
 
   return 0;
 }
